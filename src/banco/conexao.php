81511c185805ec547d5090392a7fb07cdbc2183e
wndallan <wndallan1@gmail.com>
2020-06-30 11:11:24 -0300

    Estrutura MVC e conexão com o banco

---
+++ src/banco/conexao.php
@@ -0,0 +1,7 @@
+<?php
+
+    include('config.php');
+
+    $conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+
+?>
---
+++ src/banco/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    date_default_timezone_set('America/Sao_Paulo');
+
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASSWORD', '');
+    define('DBNAME', 'dbaulads');
+
+?>
